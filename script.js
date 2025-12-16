0495f7948b7d0a9dda62fc4792f14b04bc174121
AbhinavBisht07 <bishtabhinav0077@gmail.com>
2025-12-16 22:25:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function OpenAndCloseFeatures(){
+    let allElems = document.querySelectorAll(".elem");
+    let allFullElemPages = document.querySelectorAll(".fullElem");
+    // console.log(allElems);
+    
+    let backBtns = document.querySelectorAll(".fullElem .back");
+    
+    allElems.forEach(function(elem){
+        // For opening elemPage
+        elem.addEventListener("click",function(){
+            allFullElemPages[elem.id].style.display = "block";
+        });
+    
+        // For closing elemPage
+        backBtns[elem.id].addEventListener("click",function(){
+            allFullElemPages[elem.id].style.display = "none";
+        })
+    });
+}
+OpenAndCloseFeatures();
