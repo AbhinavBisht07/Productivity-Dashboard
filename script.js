560d393b2070fe215ffc0f1d95268fd3c02b8408
AbhinavBisht07 <bishtabhinav0077@gmail.com>
2026-01-02 22:28:11 +0530

    Completed todo list

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function OpenAndCloseFeatures(){
+function OpenAndCloseFeatures() {
     let allElems = document.querySelectorAll(".elem");
     let allFullElemPages = document.querySelectorAll(".fullElem");
     // console.log(allElems);
-    
+
     let backBtns = document.querySelectorAll(".fullElem .back");
-    
-    allElems.forEach(function(elem){
+
+    allElems.forEach(function (elem) {
         // For opening elemPage
-        elem.addEventListener("click",function(){
+        elem.addEventListener("click", function () {
             allFullElemPages[elem.id].style.display = "block";
         });
-    
+
         // For closing elemPage
-        backBtns[elem.id].addEventListener("click",function(){
+        backBtns[elem.id].addEventListener("click", function () {
             allFullElemPages[elem.id].style.display = "none";
         })
     });
@@ -21,4 +21,75 @@ OpenAndCloseFeatures();
 
 
 
-let form = document.querySelector(".todo-container .addTask form");
+function todoList() {
+    var currentTasks = [];
+
+    if (localStorage.getItem("currentTasks")) {
+        // console.log("Task/tasks present in localStorage");
+        currentTasks = JSON.parse(localStorage.getItem("currentTasks"));
+    } else {
+        console.log("Not a single task present in localStorage");
+    }
+
+    function renderTasks() {
+        localStorage.setItem('currentTasks', JSON.stringify(currentTasks));
+        let allTask = document.querySelector(".allTask");
+
+        let sum = "";
+
+        currentTasks.forEach(function (elem, idx) {
+            // console.log(elem);
+            sum += `<div class="task">
+                    <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+                    <button id=${idx}>Mark as completed</button>
+                </div>`
+        });
+
+        allTask.innerHTML = sum;
+    }
+    renderTasks();
+
+
+    let form = document.querySelector(".todo-container .addTask form");
+    let taskInput = document.querySelector(".addTask form #task-input");
+    let taskDetailsInput = document.querySelector(".addTask form textarea");
+    let taskCheckBox = document.querySelector(".addTask form #check ");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // console.log(taskInput.value);
+        // console.log(taskDetailsInput.value);
+        // // console.dir(taskCheckBox);
+        // console.log(taskCheckBox.checked);
+
+        currentTasks.push(
+            {
+                task: taskInput.value,
+                details: taskDetailsInput.value,
+                imp: taskCheckBox.checked,
+            }
+        );
+        renderTasks();
+
+        location.reload();
+        // taskInput.value = "";
+        // taskDetailsInput.value = "";
+        // taskCheckBox.checked = false;
+    })
+
+
+    //Delete tasks using Mark as Complete button:-
+    let markCompletedBtns = document.querySelectorAll(".allTask .task button");
+    markCompletedBtns.forEach(function (btn) {
+        btn.addEventListener("click", function () {
+            // console.log(btn.id);
+            currentTasks.splice(btn.id, 1);
+
+            renderTasks();
+            location.reload();
+        })
+    })
+}
+
+todoList();
